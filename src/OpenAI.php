53230ba5af80b02a77593bc2ea0135aaadad3040
Muhammet ŞAFAK <info@muhammetsafak.com.tr>
2023-12-17 18:54:50 +0300

    added license

--- src/OpenAI.php
+++ src/OpenAI.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * OpenAI API Client
+ *
+ * This file is part of OpenAI API Client.
+ *
+ * @author     Muhammet ŞAFAK <info@muhammetsafak.com.tr>
+ * @copyright  Copyright © 2023 Muhammet ŞAFAK
+ * @license    ./LICENSE  MIT
+ * @version    1.0
+ * @link       https://www.muhammetsafak.com.tr
+ */
 
 declare(strict_types=1);
 namespace MuhammetSafak\OpenAI;
--- src/Services/Interfaces/ServiceInterface.php
+++ src/Services/Interfaces/ServiceInterface.php
@@ -1,5 +1,17 @@
 <?php
+/**
+ * OpenAI API Client
+ *
+ * This file is part of OpenAI API Client.
+ *
+ * @author     Muhammet ŞAFAK <info@muhammetsafak.com.tr>
+ * @copyright  Copyright © 2023 Muhammet ŞAFAK
+ * @license    ./LICENSE  MIT
+ * @version    1.0
+ * @link       https://www.muhammetsafak.com.tr
+ */
 
+declare(strict_types=1);
 namespace MuhammetSafak\OpenAI\Services\Interfaces;
 
 use MuhammetSafak\OpenAI\OpenAI;
